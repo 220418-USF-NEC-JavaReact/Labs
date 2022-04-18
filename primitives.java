3266541a1b6c26a49aa035f425ff617d65e3622f
Parth Kikani <kikaniparth@gmail.com>
2022-04-18 16:42:10 -0500

    first day of class

---
+++ primitives.java
@@ -0,0 +1,20 @@
+class Primitives {
+
+	public static void main (String args[]){
+		
+		//boolean || 1 or 0
+		//datatype varName assignment operator valueofVar
+
+		boolean myFirstBool = true;
+		System.out.println(myFirstBool);
+
+
+		
+		int widening = 12;
+
+		System.out.println(widening);
+
+	}
+
+
+}
