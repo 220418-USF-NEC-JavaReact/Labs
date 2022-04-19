e852dc10927767b1fe53bd33381475e1f36574a9
unknown <pkiecun@gmail.com>
2022-04-19 17:46:02 -0400

    aprilNinetenthFirst

---
+++ Week1/Lab1/Primitives.java
@@ -0,0 +1,9 @@
+class Primitives{
+    boolean boo = true;
+    byte maxByte = 127;
+    short minShort = -32768;
+    int maxInt = 2147483647;
+    long javaFlex = 3000000000L;
+    float pie = 3.14159f;
+    double doubleYum = 3.14159d * 2;
+}
