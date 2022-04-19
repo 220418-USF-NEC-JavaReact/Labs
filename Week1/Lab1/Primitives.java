e768a9f396f57b6e4b6bad228be95b98c195b4c7
Parth Kikani <kikaniparth@gmail.com>
2022-04-19 10:43:06 -0500

    lab one success!

---
+++ Week1/Lab1/Primitives.java
@@ -0,0 +1,13 @@
+public class Primitives {
+   
+        boolean boo = true;
+        byte maxByte = 127;
+        short minShort = -32768;
+        int maxInt = 2147483647;
+        long javaFlex = 3000000000L;
+        float pie = (float) 3.14159;
+        double doubleYum = 6.28318;
+        
+
+    
+}
