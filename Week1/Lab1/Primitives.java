be0b565c52ef2ce0cb910f32d76a48b02d46fa6d
Johnsoni17 <65513578+Johnsoni17@users.noreply.github.com>
2022-04-19 17:25:54 -0400

    finished Lab1

---
+++ Week1/Lab1/Primitives.java
@@ -0,0 +1,17 @@
+class Primitives {
+     
+        boolean boo = true;
+        
+        byte maxByte = 127;
+        
+        short minShort = -32768;
+        
+        int maxInt = 2_147_483_647;
+        
+        long javaFlex = 3_000_000_000l;
+        
+        float pie = 3.14159f;
+        
+        double doubleYum = 3.14159*2;
+
+}
