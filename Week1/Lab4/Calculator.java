c321d1fe7ab19a53986568cbc6e16cf612bca39e
jj-link <joseph.j.link@gmail.com>
2022-04-19 17:45:52 -0400

    Lab 4 success!

---
+++ Week1/Lab4/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator{
+    //add function to add together 2 integers
+    int add(int int1, int int2) {
+        return int1 + int2;
+    }
+
+    //sub function which subtracts 2 integers
+    int sub(int int1, int int2){
+        return int1 - int2;
+    }
+
+    //multiply function to multiply 2 doubles
+    double multiply(double dub1, double dub2){
+        return dub1 * dub2;
+    }
+
+    //divide function to divide 2 doubles
+    double divide(double dub1, double dub2){
+        return dub1 / dub2;
+    }
+
+}
