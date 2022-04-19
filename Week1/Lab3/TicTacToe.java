0eb6cae36dd7a4ee7bd5adc7f8d19b87e829a5b5
jj-link <joseph.j.link@gmail.com>
2022-04-19 17:32:22 -0400

    Lab 3 success

--- Week1/Lab3/TicTacToe.java
+++ Week1/Lab3/TicTacToe.java
@@ -4,6 +4,20 @@ public class TicTacToe {
 	// Write the code for the lab in the method below
 	public void playTicTacToe() {
 
+		//Initialize 3x3 tictactoe board with underscores
+		char[][] tictactoe = {{'_', '_', '_'},
+							  {'_', '_', '_'},
+							  {'_', '_', '_'}};
+
+		//implement moves of players X, O
+		tictactoe[1][1] = 'X';
+		tictactoe[1][0] = 'O';
+		tictactoe[0][2] = 'X';
+		tictactoe[2][0] = 'O';
+		tictactoe[0][0] = 'X';
+		tictactoe[2][1] = 'O';
+		tictactoe[0][1] = 'X';
+
 		// Code for the test runner, do not change
 		this.resultArr = tictactoe;
 	}
