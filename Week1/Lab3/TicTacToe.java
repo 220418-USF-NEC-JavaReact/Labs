df22bfe1035fb3576b7bda7d2123926fe92fd7bb
jtsuch <37488788+jtsuch@users.noreply.github.com>
2022-04-19 14:41:07 -0700

    Finished Lab3

--- Week1/Lab3/TicTacToe.java
+++ Week1/Lab3/TicTacToe.java
@@ -4,6 +4,15 @@ public class TicTacToe {
 	// Write the code for the lab in the method below
 	public void playTicTacToe() {
 
+		char[][] tictactoe = {{'_','_','_'},{'_','_','_'},{'_','_','_'}};
+		tictactoe[1][1] = 'X';
+		tictactoe[1][0] = 'O';
+		tictactoe[0][2] = 'X';
+		tictactoe[2][0] = 'O';
+		tictactoe[0][0] = 'X';
+		tictactoe[2][1] = 'O';
+		tictactoe[0][1] = 'X';
+
 		// Code for the test runner, do not change
 		this.resultArr = tictactoe;
 	}
