372ae4aade07ad5760864203bb796345c59b96b9
moehaji <mo.ali.abdulla@gmail.com>
2022-04-19 19:10:10 -0400

    Lab4 Done

---
+++ Week1/Lab4/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    public int add (int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+
+    public int sub (int numOne, int numTwo) {
+        return numOne - numTwo;
+    }
+
+    public double multiply (double numOne, double numTwo) {
+        return numOne * numTwo;
+    }
+
+    public double divide (double numOne, double numTwo) {
+        return numOne / numTwo;
+    }
+
+}
