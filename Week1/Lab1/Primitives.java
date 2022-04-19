cd52225c6e0cb6cb2c19de57b0d09be24b913c9d
jtsuch <37488788+jtsuch@users.noreply.github.com>
2022-04-19 14:21:23 -0700

    Finished Lab1

---
+++ Week1/Lab1/Primitives.java
@@ -0,0 +1,11 @@
+// Lab1
+// Justin Tsuchiyama
+public class Primitives {
+    boolean boo = true;
+    byte maxByte = 127;
+    short minShort = -32768;
+    int maxInt = 2147483647;
+    long javaFlex = 3000000000l;
+    float pie = 3.14159f;
+    double doubleYum = 6.28318;
+}
