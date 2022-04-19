93a3783a7212844fd7738d56b6e1e6d5eed2744f
dawitsalemu <70491458+dawitsalemu@users.noreply.github.com>
2022-04-19 15:23:55 -0600

    create array fib and variable fourthDigit containing the 3rd value of fib; create test driver class NumberArrayDriver

--- Week1/Lab2/NumberArray.java
+++ Week1/Lab2/NumberArray.java
@@ -2,6 +2,10 @@ public class NumberArray{
 
 	public int result;
 	public int[] resultArr;
+	// create array fib with 5 indexes
+	public int[] fib = {0, 1, 1, 2, 3};
+	// get 3rd number of fib
+	public int fourthDigit = fib[2];
 	
 	//Place the code for the lab inside of the method below
 	public void numberArray(){
---
+++ Week1/Lab2/NumberArrayDriver.java
@@ -0,0 +1,8 @@
+// NumberArrayDriver class to test NumberArray class
+class NumberArrayDriver{
+    public static void main(String args[]){
+        NumberArray numArray = new NumberArray();
+        numArray.numberArray();
+        System.out.println(numArray.result);
+    }
+}
