cc07b1a7b52ab6a4c4d5450ceae4f116aaa8b0d8
jtsuch <37488788+jtsuch@users.noreply.github.com>
2022-04-19 14:48:20 -0700

    Finished Lab4

---
+++ Week1/Lab4/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+}
