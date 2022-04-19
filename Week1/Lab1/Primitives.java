7bdefdf2fd2fde0a3cd8807eb5ee38b5379c1ef2
jj-link <joseph.j.link@gmail.com>
2022-04-19 12:15:25 -0400

    completed Primitives Class

---
+++ Week1/Lab1/Primitives.java
@@ -0,0 +1,18 @@
+
+public class Primitives {
+
+    boolean boo = true;
+
+    byte maxByte = 127;
+
+    short minShort = -32_768;
+
+    int maxInt = 2147483647;
+
+    long javaFlex = 3_000_000_000L;
+
+    float pie = 3.14159F;
+
+    double doubleYum = 3.14159 * 2;
+
+}
