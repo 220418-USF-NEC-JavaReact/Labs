ba8fd7d94117ceee11b38bc147e99c271c58dec0
jtsuch <37488788+jtsuch@users.noreply.github.com>
2022-04-20 15:11:55 -0700

    Finished Lab8

--- Week1/Lab8/StringReverser.java
+++ Week1/Lab8/StringReverser.java
@@ -1,4 +1,11 @@
 public class StringReverser {
 
 	// Create the method to reverse a string below
+	public String reverse(String s) {
+		StringBuilder sb = new StringBuilder();
+		for(int i = s.length()-1; i >= 0; i--) {
+			sb.append(s.charAt(i));
+		}
+		return sb.toString();
+	}
 }
