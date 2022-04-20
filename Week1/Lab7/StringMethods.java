69abe1b9bbe54f10c92779c732b0283f5c5a5efb
Johnsoni17 <65513578+Johnsoni17@users.noreply.github.com>
2022-04-20 17:10:34 -0400

    finished Lab7

--- Week1/Lab7/StringMethods.java
+++ Week1/Lab7/StringMethods.java
@@ -1,3 +1,27 @@
 public class StringMethods {
 
+    public String replaceAllSpaces(String str){
+
+        char[] chars = str.toCharArray();
+
+        for(int index = 0; index < str.length(); index++){
+            if(chars[index]==' '){
+                chars[index] = '-';
+            }
+        }
+
+        return String.valueOf(chars);
+    }
+
+    public boolean containsWord(String input, String search){
+
+        input = input.toLowerCase();
+        return input.contains(search);
+    }
+
+    public String[] splitPhoneNumber(String pNumber){
+
+        return pNumber.split("-");
+    }
+
 }
