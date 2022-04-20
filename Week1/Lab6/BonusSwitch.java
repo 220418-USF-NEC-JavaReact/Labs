397c1bccf6161400e657c5c4766550b9e737ad33
bayodes <bayode.o.olaoye@gmail.com>
2022-04-20 17:09:58 -0400

    Completed Lab6

--- Week1/Lab6/BonusSwitch.java
+++ Week1/Lab6/BonusSwitch.java
@@ -4,7 +4,30 @@ public class BonusSwitch {
 	public int[] resultBonuses;
 
 	public void bonus() {
+		int[] bonuses = new int[employeeLetters.length];
+		int counter = 0;
 
+		do {
+			switch (employeeLetters[counter]) {
+				case 'A':
+					bonuses[counter] = 5000;
+					break;
+				case 'B':
+					bonuses[counter] = 2500;
+					break;
+				case 'C':
+					bonuses[counter] = 1000;
+					break;
+				case 'D':
+					bonuses[counter] = 500;
+					break;
+				case 'F':
+					bonuses[counter] = 0;
+					break;
+			}
+			counter++;
+		} while (employeeLetters.length > counter);
+		
 		// Code for the test runner, do not change
 		resultBonuses = bonuses;
 	}
