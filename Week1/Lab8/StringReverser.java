41f7b5b79d0ac4a6ca2118315987f542b1f1ceca
unknown <pkiecun@gmail.com>
2022-04-20 09:39:09 -0400

    mightNotBcorrect

--- Week1/Lab8/StringReverser.java
+++ Week1/Lab8/StringReverser.java
@@ -1,4 +1,13 @@
 public class StringReverser {
-
+	public String reverse(String a){
+		char[] b = a.toCharArray();
+		for(int c = 0; c < b.length/2; c++){
+			char temp = b[c];
+			b[c] = b[b.length - 1 - c];
+			b[b.length -1 -c] = temp;
+		}
+		String d = String.valueOf(b);
+		return d;
+	}
 	// Create the method to reverse a string below
 }
