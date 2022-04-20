551126bc45efb56a3dd18eb5505d1438267aef2b
bayodes <bayode.o.olaoye@gmail.com>
2022-04-20 18:24:40 -0400

    Completed Lab7

--- Week1/Lab7/StringMethods.java
+++ Week1/Lab7/StringMethods.java
@@ -1,3 +1,17 @@
 public class StringMethods {
 
+    public String replaceAllSpaces(String s){
+        String newString = s.replace(' ', '-');
+        return newString;
+    }
+
+    public boolean containsWord(String s, String s2){
+        return (s.toLowerCase()).contains(s2);
+    }
+
+    public String[] splitPhoneNumber(String s){
+        String[] number;
+        number = s.split("-");
+        return number;
+    }
 }
