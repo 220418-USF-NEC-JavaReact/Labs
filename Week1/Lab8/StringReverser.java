964b523a02b7162b12327e61d7237fcd329427cf
robpduo <robpduo@gmail.com>
2022-04-20 18:02:40 -0400

    Lab8 Done

--- Week1/Lab8/StringReverser.java
+++ Week1/Lab8/StringReverser.java
@@ -1,4 +1,13 @@
 public class StringReverser {
 
 	// Create the method to reverse a string below
+	public String reverse( String curString ) {
+		StringBuilder buffString = new StringBuilder();
+
+		for (int i = curString.length() - 1; i >= 0; i--) {
+			buffString.append(curString.charAt(i));
+		}
+
+		return buffString.toString();
+	}
 }
