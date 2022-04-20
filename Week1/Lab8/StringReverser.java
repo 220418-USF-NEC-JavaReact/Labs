3496c52b9e7b42780bf97aaea9ab4e7bd26652a4
bayodes <bayode.o.olaoye@gmail.com>
2022-04-20 19:05:47 -0400

    Completed Lab8

--- Week1/Lab8/StringReverser.java
+++ Week1/Lab8/StringReverser.java
@@ -1,4 +1,15 @@
 public class StringReverser {
 
 	// Create the method to reverse a string below
+	public String reverse(String s) {
+		String s2 = "";
+		char c;
+
+		for(int i=0; i<s.length(); i++){
+			c=s.charAt(i);
+			s2 = c + s2;
+		}
+
+		return s2;
+	}
 }
