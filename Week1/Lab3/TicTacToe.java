d8c34966e1e44b46bbb0df61805113d5621d8030
unknown <pkiecun@gmail.com>
2022-04-19 21:20:25 -0400

    aprilNinetenthThird

--- Week1/Lab3/TicTacToe.java
+++ Week1/Lab3/TicTacToe.java
@@ -3,7 +3,19 @@ public class TicTacToe {
 
 	// Write the code for the lab in the method below
 	public void playTicTacToe() {
-
+		char[][] tictactoe = new char[3][3];
+		for(int a = 0; a < 3; a++){
+			for(int b = 0; b < 3; b++){
+				tictactoe[a][b] = '_';
+			}
+		}
+		tictactoe[1][1] = 'X';
+		tictactoe[1][0] = 'O';
+		tictactoe[0][2] = 'X';
+		tictactoe[2][0] = 'O';
+		tictactoe[0][0] = 'X';
+		tictactoe[2][1] = 'O';
+		tictactoe[0][1] = 'X';
 		// Code for the test runner, do not change
 		this.resultArr = tictactoe;
 	}
