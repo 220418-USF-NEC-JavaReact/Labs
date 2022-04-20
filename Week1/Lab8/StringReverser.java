bec6961e2900680e59817691d0c9097a96bfafea
Johnsoni17 <65513578+Johnsoni17@users.noreply.github.com>
2022-04-20 17:47:50 -0400

    finished Lab8

--- Week1/Lab8/StringReverser.java
+++ Week1/Lab8/StringReverser.java
@@ -1,4 +1,16 @@
 public class StringReverser {
 
 	// Create the method to reverse a string below
+	public String reverse (String str) {
+       
+		String StringBuffer="";
+        char ch;
+
+		for (int index=0; index<str.length(); index++)
+      	{
+        	ch= str.charAt(index);
+        	StringBuffer= ch+StringBuffer;
+      	}
+	  	return StringBuffer;
+    }
 }
