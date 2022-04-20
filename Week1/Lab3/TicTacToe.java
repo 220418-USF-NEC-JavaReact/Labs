f66022cfb0c8431b4a930e80310af45226b4c4a7
bayodes <bayode.o.olaoye@gmail.com>
2022-04-20 16:00:58 -0400

    Completed Lab3

--- Week1/Lab3/TicTacToe.java
+++ Week1/Lab3/TicTacToe.java
@@ -3,6 +3,16 @@ public class TicTacToe {
 
 	// Write the code for the lab in the method below
 	public void playTicTacToe() {
+		//Initialize 3x3 tictactoe board with underscores
+		char[][] tictactoe = {{'_','_','_'}, {'_','_','_'}, {'_','_','_'}}; 
+		
+		tictactoe[1][1] = 'X';
+		tictactoe[1][0] = 'O';
+		tictactoe[0][2] = 'X';
+		tictactoe[2][0] = 'O';
+		tictactoe[0][0] = 'X';
+		tictactoe[2][1] = 'O';
+		tictactoe[0][1] = 'X';
 
 		// Code for the test runner, do not change
 		this.resultArr = tictactoe;
