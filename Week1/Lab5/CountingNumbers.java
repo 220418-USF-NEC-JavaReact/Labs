3e7e1e2b88c15e128ee057062e93988766555e43
bayodes <bayode.o.olaoye@gmail.com>
2022-04-20 14:06:22 -0400

    Completed Lab5

--- Week1/Lab5/CountingNumbers.java
+++ Week1/Lab5/CountingNumbers.java
@@ -5,6 +5,23 @@ public class CountingNumbers {
 
 	// Write the code for this lab in the method below
 	public void countNumbers() {
+		int[] numbers = new int[100];
+		int evenSum = 0;
+		int oddSum = 0;
+		int counter = 0;
+
+		for(int i =0; i<100; i++) {
+			numbers[i] = i+1;
+		}
+
+		while (counter < numbers.length ) {
+			if(numbers[counter] % 2 == 0) {
+				evenSum = numbers[counter] + evenSum;
+			} else {
+				oddSum = numbers[counter] + oddSum;
+			}
+			counter++;
+		}
 
 		// Code for test runner, do not change
 		evenResult = evenSum;
