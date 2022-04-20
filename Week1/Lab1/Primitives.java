86e0f5d3dccdaceb8f7869c9d3bd4cc32458db65
S1m1ster <darrylrobles14@gmail.com>
2022-04-20 08:04:48 -0400

    Lab1 added

---
+++ Week1/Lab1/Primitives.java
@@ -0,0 +1,25 @@
+class Primitives {
+
+    public static void main(String args[]){
+        Boolean boo = true;
+
+        Byte maxByte = 127;
+
+        Short minShort = -32768;
+
+        int maxInt = 2147483647;
+
+        long javaFlex = 3000000000L;
+
+        float pie = Math.round(3.14159);
+
+        double doubleYum = pie *2;
+        System.out.println(pie);
+
+
+
+
+    }
+
+
+}
