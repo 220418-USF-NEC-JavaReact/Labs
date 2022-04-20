adfb1b1db9abbe58031136a0a5b3562302d6ddd7
jayab2010 <jayab2010@gmail.com>
2022-04-20 00:48:47 -0400

    Lab1 Done

---
+++ Week1/Lab1/Primitives.java
@@ -0,0 +1,13 @@
+package LABS.Week1.Lab1;
+
+public class Primitives {
+    boolean boo = true;
+    byte maxByte = 127;
+    short minShort = -32768;
+    int maxInt = 2147483647;
+    long javaFlex = 3000000000L;
+    float pie = 3.14159f;
+    double doubleYum = 3.14159d * 2;
+    
+    
+}
