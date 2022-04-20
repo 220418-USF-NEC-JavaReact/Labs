d2879e98f9fd42889f7320f2a380d06e9d019052
nabilgj <nabilgj.bnm@gmail.com>
2022-04-20 07:15:59 -0400

    Lab2 completed

--- Week1/Lab2/NumberArray.java
+++ Week1/Lab2/NumberArray.java
@@ -5,9 +5,12 @@ public class NumberArray{
 	
 	//Place the code for the lab inside of the method below
 	public void numberArray(){
+				int fib[] = {0, 1, 1, 2, 3};
+				int fourthDigit = fib[2];
+				System.out.println("third number of the fibonacci sequence " + fourthDigit);
 
-		//Code for the test runner, do not change
-		result = fourthDigit;
-		resultArr = fib;
+				//Code for the test runner, do not change
+				result = fourthDigit;
+				resultArr = fib;
 	}
 }
