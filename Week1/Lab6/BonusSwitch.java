d80df73861b4ee35a79ef925c8f06acd1593a17c
unknown <pkiecun@gmail.com>
2022-04-19 22:09:01 -0400

    aprilNinetenthSixth

--- Week1/Lab6/BonusSwitch.java
+++ Week1/Lab6/BonusSwitch.java
@@ -4,7 +4,29 @@ public class BonusSwitch {
 	public int[] resultBonuses;
 
 	public void bonus() {
-
+		int[] bonuses = new int[employeeLetters.length];
+		int a = 0;
+		do{
+			switch(employeeLetters[a]){
+			case 'A':
+				bonuses[a] = 5000;
+				break;
+			case 'B':
+				bonuses[a] = 2500;
+				break;
+			case 'C':
+				bonuses[a] = 1000;
+				break;
+			case 'D':
+				bonuses[a] = 500;
+				break;
+			case 'F':
+				bonuses[a] = 0;
+				break;
+			default :
+			}
+			a++;
+		}while(a < bonuses.length);
 		// Code for the test runner, do not change
 		resultBonuses = bonuses;
 	}
