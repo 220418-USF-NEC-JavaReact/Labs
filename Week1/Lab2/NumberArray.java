ef3e8da696d44786b19852d4534a0c2d76a15ab2
S1m1ster <darrylrobles14@gmail.com>
2022-04-20 08:50:04 -0400

    Lab 2 completed

--- Week1/Lab2/NumberArray.java
+++ Week1/Lab2/NumberArray.java
@@ -1,13 +1,25 @@
 public class NumberArray{
 
-	public int result;
+	public int result, fourthDigit;
 	public int[] resultArr;
 	
 	//Place the code for the lab inside of the method below
 	public void numberArray(){
+
+		int[] fib = new int[5];
+
+		fib[0] = 0;
+		fib[1] = 1;
+		fib[2] = 1;
+		fib[3] = 2;
+		fib[4] = 3;
+
+		fourthDigit = fib[3];
 
 		//Code for the test runner, do not change
 		result = fourthDigit;
 		resultArr = fib;
 	}
 }
+
+
