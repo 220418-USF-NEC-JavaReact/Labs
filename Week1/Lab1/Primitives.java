9d2053ddf15b9b8b49e3c55092c8618cd4fbb994
bayodes <bayode.o.olaoye@gmail.com>
2022-04-20 06:21:42 -0400

    Completed Lab1

---
+++ Week1/Lab1/Primitives.java
@@ -0,0 +1,21 @@
+public class Primitives {
+    
+    public static void main(String[] args) {
+        boolean boo = true;
+        byte maxByte = 127;
+        short minShort = -32768;
+        int maxInt = 2147483647;
+        long javaFlex = 9223372036854775807l; // 
+        float pie = 3.1415f;
+        double doubleYum = 3.1415 * (2);
+
+        System.out.println(boo);
+        System.out.println(maxByte);
+        System.out.println(minShort);
+        System.out.println(maxInt);
+        System.out.println(javaFlex);
+        System.out.println(pie);
+        System.out.println(doubleYum);
+    }
+    
+}
