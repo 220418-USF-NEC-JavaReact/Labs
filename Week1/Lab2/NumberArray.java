c1d03a58a61af5c580ea9fc7789c938b1446e1cf
swathij943 <swathij943@gmail.com>
2022-04-21 00:21:44 -0400

    Lab2 done

--- Week1/Lab2/NumberArray.java
+++ Week1/Lab2/NumberArray.java
@@ -5,6 +5,8 @@ public class NumberArray{
 	
 	//Place the code for the lab inside of the method below
 	public void numberArray(){
+		int fib[] = {0, 1, 1, 2, 3};
+		int fourthDigit = fib[3];
 
 		//Code for the test runner, do not change
 		result = fourthDigit;
