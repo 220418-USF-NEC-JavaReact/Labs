ab7830da447b9a5c2245cae6364e6ebc6d2209d7
Milan Verma <millan.verma@gmail.com>
2022-04-21 15:30:46 -0600

    Lab 8 complete

--- Week1/Lab8/StringReverser.java
+++ Week1/Lab8/StringReverser.java
@@ -1,4 +1,13 @@
 public class StringReverser {
 
 	// Create the method to reverse a string below
+	public String reverse(String str){
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=str.length()-1;i>=0;i--){
+			sb.append(str.charAt(i));
+		}
+		
+		return sb.toString();
+	}
 }
