34b0f6ebc6a2042f5c3bf00836d6d37d235dcc89
Parth Kikani <kikaniparth@gmail.com>
2022-04-21 17:05:32 -0500

    lab5 success

--- Week1/Lab5/CountingNumbers.java
+++ Week1/Lab5/CountingNumbers.java
@@ -1,10 +1,33 @@
 public class CountingNumbers {
-
+	int evenSum;
+	int oddSum;
 	int evenResult;
 	int oddResult;
 
 	// Write the code for this lab in the method below
 	public void countNumbers() {
+
+		int[] numbers = new int[100];
+
+		for (int i=0;i<numbers.length;i++){
+			numbers[i] = i + 1;
+			System.out.println(numbers[i]);
+		}
+		int counter = 0;
+		while (counter<100){
+			if(numbers[counter]%2 == 0){
+				evenSum += numbers[counter];
+
+
+			}
+			else{
+				oddSum += numbers[counter];
+			}
+			counter++;
+		}
+
+
+
 
 		// Code for test runner, do not change
 		evenResult = evenSum;
