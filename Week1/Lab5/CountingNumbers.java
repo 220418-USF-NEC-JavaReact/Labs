93d5af85c49bddd85a04b5c81e2a475b3f840d45
Hassan Zareef <hassan.zareef@gmail.com>
2022-04-21 18:01:24 -0400

    Lab 5 Challenge Completed

--- Week1/Lab5/CountingNumbers.java
+++ Week1/Lab5/CountingNumbers.java
@@ -2,10 +2,26 @@ public class CountingNumbers {
 
 	int evenResult;
 	int oddResult;
+	int count =0 ;
 
 	// Write the code for this lab in the method below
 	public void countNumbers() {
+	int numbers[] = new int[100];
+	for(int i = 0; i<100; i++){
+		numbers[i] = i + 1;
 
+	}
+
+	int evenSum = 0;
+	int oddSum = 0;
+	while(count < 100){
+		if(numbers[count] % 2 == 0){
+			evenSum = evenSum + numbers[count];
+		} else {
+			oddSum = oddSum + numbers[count];
+		}
+		count++;
+	}
 		// Code for test runner, do not change
 		evenResult = evenSum;
 		oddResult = oddSum;
