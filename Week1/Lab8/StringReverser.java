2f5836512d1b1f7af843495d48c3fa87ff0ba94e
dawitsalemu <70491458+dawitsalemu@users.noreply.github.com>
2022-04-21 07:27:36 -0600

    create string reversing method using for loop; display reversed string in the method

--- Week1/Lab8/StringReverser.java
+++ Week1/Lab8/StringReverser.java
@@ -1,4 +1,18 @@
 public class StringReverser {
 
-	// Create the method to reverse a string below
+	// reverse a string
+	public String reverse(String str){
+		StringBuffer sb = new StringBuffer();
+		for (int i = str.length() - 1; i >= 0; i--)
+		{
+			sb.append(str.charAt(i));
+		}
+		String result = sb.toString();
+		return result;
+	}
+
+	public static void main(String args[]){
+		StringReverser str = new StringReverser();
+		System.out.println(str.reverse("Word"));
+	}
 }
