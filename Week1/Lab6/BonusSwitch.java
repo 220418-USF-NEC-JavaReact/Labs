5cec8581cc1b9104efe7ac1e2e20b6d6ac007212
Milan Verma <millan.verma@gmail.com>
2022-04-20 20:27:53 -0600

    lab 6 completed

--- Week1/Lab6/BonusSwitch.java
+++ Week1/Lab6/BonusSwitch.java
@@ -5,6 +5,32 @@ public class BonusSwitch {
 
 	public void bonus() {
 
+		int size = employeeLetters.length;
+		int[] bonuses = new int[size];
+		int index = 0;
+
+		do {
+			switch (employeeLetters[index]) {
+				case 'A':
+					bonuses[index] = 5000;
+					break;
+				case 'B':
+					bonuses[index] = 2500;
+					break;
+				case 'C':
+					bonuses[index] = 1000;
+					break;
+				case 'D':
+					bonuses[index] = 500;
+					break;
+				case 'F':
+					bonuses[index] = 0;
+					break;
+			}
+
+			index++;
+		} while (index < size);
+
 		// Code for the test runner, do not change
 		resultBonuses = bonuses;
 	}
