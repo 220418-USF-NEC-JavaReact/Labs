cb949836f398aa2d519df5476009da9d4e13fb0b
moehaji <mo.ali.abdulla@gmail.com>
2022-04-21 18:01:33 -0400

    Lab7 Done

--- Week1/Lab7/StringMethods.java
+++ Week1/Lab7/StringMethods.java
@@ -1,3 +1,18 @@
 public class StringMethods {
 
+    public String replaceAllSpaces(String replacedStr) {
+        return replacedStr.replace(" ", "-");
+    }
+
+    public boolean containsWord(String firstStr, String secondStr) {
+        String firstLowerCase = firstStr.toLowerCase();
+        String secondLowerCase = secondStr.toLowerCase();
+
+        return firstLowerCase.contains(secondLowerCase);
+    }
+
+    public String[] splitPhoneNumber(String phoneNumber) {
+        return phoneNumber.split("-");
+    }
+
 }
