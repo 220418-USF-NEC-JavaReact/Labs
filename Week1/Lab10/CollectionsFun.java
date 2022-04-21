b9cc727284db60f790fa229557730cae8893e021
unknown <pkiecun@gmail.com>
2022-04-21 18:19:13 -0400

    aprilTwentyFirstTen

--- Week1/Lab10/CollectionsFun.java
+++ Week1/Lab10/CollectionsFun.java
@@ -8,5 +8,28 @@ import java.util.Iterator;
 public class CollectionsFun {
 
 	// Fill out the class with the logic
-
+	public List<String> nameList(List<Person> pop){
+		List<String> first = new ArrayList<>();
+		for(Person p : pop){
+			first.add(p.getFirst());
+		}
+		return first;
+	}
+	public Map<String, Person> personMap(List<Person> a){
+		Map<String, Person> plot = new HashMap<>();
+		for(Person b : a){
+			plot.put(b.getFirst().concat(b.getLast()), b);
+		}
+		return plot;
+	}
+	public Person iteratorFun(List<Person> pop){
+		Iterator<Person> census = pop.iterator();
+		Person oldest = census.next();
+			while(census.hasNext()){
+				if(census.next().getAge() > oldest.getAge()){
+				oldest = census.next();
+			}
+		}
+		return oldest;
+	}
 }
