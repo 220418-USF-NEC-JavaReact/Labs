4e6df0994047f6d2f3cd8c619e196afe8e0b5025
Parth Kikani <kikaniparth@gmail.com>
2022-04-21 16:39:51 -0500

    lab4 Success

---
+++ Week1/Lab4/Calculator.java
@@ -0,0 +1,21 @@
+class Calculator{
+
+    public int add(int x, int y){
+        return x+y;
+    }
+
+    public int sub(int x, int y){
+        return x - y;
+    }
+
+    public double multiply(double x, double y) {
+        return x*y;
+    }
+
+    public double divide(double x, double y){
+        return x/y;
+    }
+
+
+
+}
