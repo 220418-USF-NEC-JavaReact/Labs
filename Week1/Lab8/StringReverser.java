b55f454d670f76d7388d44710125ae4418ac6a30
moehaji <mo.ali.abdulla@gmail.com>
2022-04-20 22:12:37 -0400

    Lab8 Done

--- Week1/Lab8/StringReverser.java
+++ Week1/Lab8/StringReverser.java
@@ -1,4 +1,13 @@
 public class StringReverser {
 
 	// Create the method to reverse a string below
+	public String reverse(String str) {
+		StringBuilder reversedStr = new StringBuilder();
+		for (int i = str.length() - 1; i >= 0; i--) {
+			reversedStr.append(str.charAt(i));
+		}
+
+		return reversedStr.toString();
+	}
+
 }
