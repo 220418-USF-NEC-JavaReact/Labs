d6ca7603ea65cd1e5c40b23179e0c78e2708f173
Parth Kikani <kikaniparth@gmail.com>
2022-04-21 17:20:03 -0500

    lab6 success

--- Week1/Lab6/BonusSwitch.java
+++ Week1/Lab6/BonusSwitch.java
@@ -1,9 +1,36 @@
 public class BonusSwitch {
-
+	
 	public char[] employeeLetters;
 	public int[] resultBonuses;
 
+
+
 	public void bonus() {
+		 int[] bonuses = new int[employeeLetters.length];
+
+		 int counter = 0;
+		 do{
+			 switch(employeeLetters[counter]){
+				case 'A':
+				 bonuses[counter] = 5000;
+				 	break;
+				case 'B':
+				bonuses[counter] = 2500;
+					break;
+				case 'C':
+				bonuses[counter] = 1000;
+					break;
+				case 'D':
+				bonuses[counter] = 500;
+					break;
+				case 'F':
+				bonuses[counter] = 0;
+
+			 }
+			 counter++;
+			 	
+		 } while(counter< employeeLetters.length);
+
 
 		// Code for the test runner, do not change
 		resultBonuses = bonuses;
