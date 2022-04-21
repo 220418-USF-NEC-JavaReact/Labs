6d5a42d81b9bc05c79878c8eb9cf19e97c87e071
dawitsalemu <70491458+dawitsalemu@users.noreply.github.com>
2022-04-21 06:57:41 -0600

    replace spaces with '-'; search a word; split string at '-'

--- Week1/Lab7/StringMethods.java
+++ Week1/Lab7/StringMethods.java
@@ -1,3 +1,19 @@
 public class StringMethods {
+    // replace spaces with dashes
+    public String replaceAllSpaces(String x){
+        x.replace(' ', '-');
+        return x;
+    }
 
+    // search word
+    public boolean containsWord(String inputWord, String searchWord){
+        inputWord.toLowerCase();
+        return inputWord.contains(searchWord);
+    }
+
+    // split string at '-' char
+    public String[] splitPhoneNumber(String phoneNumber){
+        String[] result = phoneNumber.split("-");
+        return result;
+    }
 }
