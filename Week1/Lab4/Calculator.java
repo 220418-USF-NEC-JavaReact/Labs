a80190554b2fa761478fa7fd7e20b23c9e88861d
Hassan Zareef <hassan.zareef@gmail.com>
2022-04-21 17:47:19 -0400

    Lab 3 and Lab 4 Challenge Completed

---
+++ Week1/Lab4/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator{
+
+    public int add(int x, int y){
+        
+        return x + y;
+
+    }
+public int sub(int x, int y){
+        
+        return x - y;
+
+    }
+
+public double multiply(double x, double y){
+        
+        return x * y;
+
+    }
+
+public double divide(double x, double y){
+        
+        return x / y;
+
+    }
+}
