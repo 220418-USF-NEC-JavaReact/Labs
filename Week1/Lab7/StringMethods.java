c7f70649ca0d0dbafd98f1c8686889ee5f973ed8
Milan Verma <millan.verma@gmail.com>
2022-04-20 20:44:57 -0600

    lab7 complete

--- Week1/Lab7/StringMethods.java
+++ Week1/Lab7/StringMethods.java
@@ -1,3 +1,20 @@
 public class StringMethods {
+    public String replaceAllSpaces(String str) {
+        str = str.replaceAll(" ", "-");
+        return str;
+    }
 
+    public boolean containsWord(String first, String second) {
+        first = first.toLowerCase();
+        second = second.toLowerCase();
+        if (first.contains(second)) {
+            return true;
+        } else{
+            return false;
+        }        
+    }
+
+    public String[] splitPhoneNumber(String phno) {
+        return phno.split("-");
+    }
 }
