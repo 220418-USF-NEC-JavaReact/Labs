3ba1e77db5bd2c0d7194cfbade1d808a8ce7c2e8
S1m1ster <darrylrobles14@gmail.com>
2022-04-22 09:26:07 -0400

    Completed lab4

---
+++ Week1/Lab4/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    
+    public int add(int x, int z){
+        return x + z;
+    }
+
+    public int sub(int x, int z){
+        return x - z;
+    }
+
+    public double multiply(double x, double z){
+        return x * z;
+    }
+
+    public double divide(double x, double z){
+        return x / z;
+    }
+
+}
