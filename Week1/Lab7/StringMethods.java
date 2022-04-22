eb8c0ec0b297a2d1f0df36151b7e4d9ded902d93
jayab2010 <jayab2010@gmail.com>
2022-04-21 20:42:09 -0400

    lab7 Done

--- Week1/Lab7/StringMethods.java
+++ Week1/Lab7/StringMethods.java
@@ -1,3 +1,26 @@
 public class StringMethods {
 
+    //Replace all the spaces to dash
+    public String replaceAllSpaces(String x){
+        x.replace(' ', '-');
+        return x;
+    }
+      
+    //search word
+    public boolean containsWord(String inputWord, String searchWord){
+       inputWord.toLowerCase();
+       return inputWord.contains(searchWord);
+     }
+
+    //split String into an array
+     public String[] splitPhoneNumber(String phoneNumber){
+        String[] result = phoneNumber.split("-");
+        return result;
+
+     }
+
+
+
+
+
 }
