37ca96c899ec4774448885588656b413f45af0c9
S1m1ster <darrylrobles14@gmail.com>
2022-04-22 09:25:17 -0400

    Lab3 completed

--- Week1/Lab3/TicTacToe.java
+++ Week1/Lab3/TicTacToe.java
@@ -4,6 +4,30 @@ public class TicTacToe {
 	// Write the code for the lab in the method below
 	public void playTicTacToe() {
 
+		char[][] tictactoe = { {'_', '_', '_'}, {'_', '_', '_'}, {'_', '_', '_'}};
+
+		//Player x chooses the center
+		tictactoe[1][1] = 'X';
+
+		//Player o chooses middle row left column
+		tictactoe[1][0] = 'O';
+
+		//Player X plays in the top row right column
+		tictactoe[0][2] = 'X';
+		
+		//Player O plays in the bottom row left column
+		tictactoe[2][0] = 'O';
+	  	
+		//Player X plays in the top row left column
+		tictactoe[0][0] = 'X';
+	  	
+		//Player O plays in the bottom row middle column
+		tictactoe[2][1] = 'O';
+	   	
+		//Player X plays in the top row middle column and WINS
+		tictactoe[0][1] = 'X';
+
+
 		// Code for the test runner, do not change
 		this.resultArr = tictactoe;
 	}
