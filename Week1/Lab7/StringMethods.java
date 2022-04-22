59f6e96ba67c269729ebb613eb0389a4f629c800
Parth Kikani <kikaniparth@gmail.com>
2022-04-21 22:00:07 -0500

    Lab7 success

--- Week1/Lab7/StringMethods.java
+++ Week1/Lab7/StringMethods.java
@@ -1,3 +1,25 @@
 public class StringMethods {
 
+    public String replaceAllSpaces(String args){
+
+        String replacedString = args.replace(" ", "-");
+        return replacedString;
+
+
+    }
+
+    public boolean containsWord(String args, String arg) {
+       String firstString = args.toLowerCase();
+       String secondString = arg.toLowerCase();
+       boolean answer = firstString.contains(secondString);
+         
+        return answer;
+    }
+
+    public String[] splitPhoneNumber(String phonenumber){
+         
+        String[] splitNumber = phonenumber.split("-");
+        return splitNumber;
+    }
+
 }
