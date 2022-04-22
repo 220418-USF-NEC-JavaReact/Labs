7594f56d976540c271ba61e2e2492529c027fe80
Johnsoni17 <65513578+Johnsoni17@users.noreply.github.com>
2022-04-22 12:53:44 -0400

    finished Lab10

--- Week1/Lab10/CollectionsFun.java
+++ Week1/Lab10/CollectionsFun.java
@@ -8,5 +8,37 @@ import java.util.Iterator;
 public class CollectionsFun {
 
 	// Fill out the class with the logic
+    public List<String> nameList(List<Person> pList){
+
+        List<String> firsts = new ArrayList<String>();
+        for(Person temp:pList){
+            firsts.add(temp.getFirst());
+        }
+        return firsts;
+    }
+
+    public Map<String, Person> personMap(List<Person> pList){
+
+        Map<String, Person> pm = new HashMap<>();
+        for(Person temp:pList){
+            pm.put(temp.getFirst()+temp.getLast(),temp);
+        }
+        return pm;
+    }
+
+    public Person iteratorFun(List<Person> pList){
+
+        Iterator<Person> pIterator = pList.iterator();
+        Person old;
+        old = pIterator.next();
+
+        while(pIterator.hasNext()){
+            Person temp = pIterator.next();
+            if (temp.getAge() > old.getAge()){
+                old = temp;
+            }
+        }
+        return  old;
+    }
 
 }
