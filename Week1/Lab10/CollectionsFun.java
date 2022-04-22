cff5c9c03369f2469c5eaec8fb2efd94f30b18ed
robpduo <robpduo@gmail.com>
2022-04-22 00:36:37 -0400

    Lab10 Complete

--- Week1/Lab10/CollectionsFun.java
+++ Week1/Lab10/CollectionsFun.java
@@ -9,4 +9,59 @@ public class CollectionsFun {
 
 	// Fill out the class with the logic
 
+	/*creates a list of first names
+	input: List<Person>
+	output: List<String> */
+	public List<String> nameList (List<Person> in) {
+		List<String> firstNames = new ArrayList();
+
+		for(Person name : in) {
+			firstNames.add(name.getFirst());
+		}
+
+		return firstNames;
+	}
+
+	/*Loops through a list of Persons and assign a key
+	input: List<Person>
+	output: Map<String, Person> */
+	public Map<String, Person> personMap (List<Person> in) {
+		Map<String, Person> catMap = new HashMap();
+
+		for(Person cat : in) {
+			catMap.put(cat.getFirst().concat(cat.getLast()), cat);
+		}
+		return catMap;
+	}
+
+	/* Looks through a list of people and returns the oldest person from the list
+	input: List<Person>
+	output: Person */
+
+	public Person iteratorFun (List<Person> in) {
+		Iterator<Person> itr = in.iterator();
+		Person buffer;
+		Person oldiLocks = new Person(null, null, 0); //there was no constructor class in person.....
+
+		//check if List<Person> exists
+		if (itr.hasNext()) {
+			buffer = itr.next();
+			oldiLocks = buffer;
+		}
+
+		while (itr.hasNext()) {
+			//retrieve the age of the next person in a buffer
+			buffer = itr.next();
+
+			//check ages of previous and current persons
+			if (buffer.getAge() > oldiLocks.getAge()) {
+				oldiLocks = buffer; //OldiLocks becomes the next person
+			}
+
+		}
+
+		return oldiLocks;
+	}
+	
+
 }
---
+++ Week1/Lab10/driver.java
@@ -0,0 +1,43 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class driver {
+    public static void main(String[] args) {
+        CollectionsFun global = new CollectionsFun();
+
+        /*task 1*/
+        List<Person> t1A = new ArrayList<Person>();
+
+        for (int i = 0; i <= 3; i ++) {
+            t1A.add(new Person("Robert" + i, "Duo", i*7));
+        }
+
+        List<String> r1 = global.nameList(t1A); 
+
+        for(int i = 0; i < r1.size(); i++) {
+            System.out.println("Names: " + r1.get(i));
+        }
+        /*task 1 complete*/
+
+        /*task 2: catMap*/
+        Map<String, Person> t2 = new HashMap();
+
+        t2 = global.personMap(t1A);
+
+        for(Map.Entry<String, Person> t2A : t2.entrySet()) {
+            System.out.println("Key: " + t2A.getKey() + " value (AGE): " + t2A.getValue().getAge());
+        }
+
+        //retrieve a value at a key
+        System.out.println("Age of Person at Key(Robert0Duo): " + t2.get("Robert0Duo").getAge());
+
+        /*task 2 complete*/
+
+        /*task 3*/
+        Person t3 = global.iteratorFun(t1A);
+
+        System.out.println("Oldest: " + t3.getAge());
+    }  
+}
