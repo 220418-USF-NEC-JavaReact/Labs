b882b9e8f5f96089f9cd202d31afc850b3f0c2a4
Parth Kikani <kikaniparth@gmail.com>
2022-04-21 22:30:27 -0500

    Lab8 Success

--- Week1/Lab8/StringReverser.java
+++ Week1/Lab8/StringReverser.java
@@ -1,4 +1,17 @@
 public class StringReverser {
-
 	// Create the method to reverse a string below
+
+
+	public String reverse(String string) {
+		StringBuilder reverseString = new StringBuilder();
+		for(int i = string.length() - 1;i >= 0;i--){
+			reverseString.append(string.charAt(i));
+		}
+		
+		String finalString = reverseString.toString();
+		
+		
+		
+		return finalString;
+	}
 }
