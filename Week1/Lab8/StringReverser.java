54341f114c4c684466014858741400dcb4989f48
jayab2010 <jayab2010@gmail.com>
2022-04-21 22:39:02 -0400

    Lab8 Done

--- Week1/Lab8/StringReverser.java
+++ Week1/Lab8/StringReverser.java
@@ -1,4 +1,20 @@
 public class StringReverser {
 
 	// Create the method to reverse a string below
+	
+	public String reverse(String str) {
+
+		StringBuilder reverseStr = new StringBuilder();
+
+		for (int i = str.length() - 1; i >= 0; i--) {
+			reverseStr.append(str.charAt(i));
+		}
+
+		return reverseStr.toString();
+	}
+
 }
+
+	 
+
+
