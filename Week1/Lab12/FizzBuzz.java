fcc9198b53bfba1042ececce9da7a3f14f226b3a
Johnsoni17 <65513578+Johnsoni17@users.noreply.github.com>
2022-04-22 15:20:34 -0400

    finished Lab12

--- Week1/Lab12/FizzBuzz.java
+++ Week1/Lab12/FizzBuzz.java
@@ -3,6 +3,17 @@ public class FizzBuzz {
     // Please place the logic to the coding problem in the method below
     public String fizzBuzz(int n) {
 
-    }
+        String ret= "";
 
+        if(n % 3 == 0 && n % 5 == 0){
+            ret = "FizzBuzz";
+        }else if(n % 3 == 0){
+            ret = "Fizz";
+        }else if(n % 5 == 0){
+            ret = "Buzz";
+        }else
+            ret = "" + n;
+
+        return ret;
+    }
 }
