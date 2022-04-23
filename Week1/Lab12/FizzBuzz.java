0bf5efd7faf641a614ef441409815220f5d069c9
bayodes <bayode.o.olaoye@gmail.com>
2022-04-23 19:25:07 -0400

    Completed Lab12

--- Week1/Lab12/FizzBuzz.java
+++ Week1/Lab12/FizzBuzz.java
@@ -2,7 +2,16 @@ public class FizzBuzz {
 
     // Please place the logic to the coding problem in the method below
     public String fizzBuzz(int n) {
+        Integer num = n;
 
+        if((n%3 == 0) & (n%5 == 0)){
+            return "FizzBuzz";
+        } else if (n%3 == 0) {
+            return "Fizz";
+        } else if(n%5 == 0) {
+            return "Buzz";
+        }
+        return num.toString();
     }
 
 }
