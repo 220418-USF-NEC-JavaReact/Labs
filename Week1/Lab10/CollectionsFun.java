1cccd61cd8f7de58385151b9516d2e09cb9b8b34
bayodes <bayode.o.olaoye@gmail.com>
2022-04-23 17:44:35 -0400

    Completed Lab10

--- Week1/Lab10/CollectionsFun.java
+++ Week1/Lab10/CollectionsFun.java
@@ -8,5 +8,36 @@ import java.util.Iterator;
 public class CollectionsFun {
 
 	// Fill out the class with the logic
+	public List<String> nameList(List<Person> person) {
+		
+		List<String> newList = new ArrayList<>();
+		for (Person p: person) {
+			newList.add(p.getFirst());
+		}
+
+		return newList;
+	}
+
+	public Map<String, Person> personMap(List <Person> people) {
+
+		Map<String, Person> m = new HashMap<>();
+		for (Person p: people) {
+			m.put(p.getFirst() + p.getLast(), p);
+		}
+
+		return m;
+	}
+
+	public Person iteratorFun(List<Person> people) {
+
+		Iterator<Person> iT = people.iterator();
+		Person oldest = iT.next();
+			while(iT.hasNext()){
+				if(iT.next().getAge() > oldest.getAge()){
+				oldest = iT.next();
+			}
+		}
+		return oldest;
+	}
 
 }
