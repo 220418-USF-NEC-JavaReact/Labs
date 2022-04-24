bbfbf91a0e70fac24a71091c5519b5662b4be831
Milan Verma <millan.verma@gmail.com>
2022-04-24 11:42:02 -0600

    Lab10 Completed

--- Week1/Lab10/CollectionsFun.java
+++ Week1/Lab10/CollectionsFun.java
@@ -8,5 +8,39 @@ import java.util.Iterator;
 public class CollectionsFun {
 
 	// Fill out the class with the logic
+	public List<String> nameList(List<Person> person){
+		List<String> names = new ArrayList<String>();
 
+		for(int i=0;i<person.size();i++){
+			names.add(person.get(i).getFirst());
+		}
+		return names;
+	}
+
+	public Map<String, Person> personMap(List<Person> person){
+		Map<String, Person> map= new HashMap<String,Person>();
+		
+		for(int i=0;i<person.size();i++){
+			map.put(person.get(i).getFirst()+person.get(i).getLast(), person.get(i));
+		}
+		return map;
+	}
+
+	public Person iteratorFun(List<Person> person){
+
+		Iterator<Person> iter = person.iterator();
+		int oldAge = 0;
+		int age = 0;
+		Person oldest = null;
+		for(int i=0;iter.hasNext();i++){
+			age = person.get(i).getAge();
+			if(oldAge<age){
+				oldAge = age;
+				oldest = iter.next();
+			} else {
+				iter.next();
+			}
+		}
+		return oldest;
+	}
 }
