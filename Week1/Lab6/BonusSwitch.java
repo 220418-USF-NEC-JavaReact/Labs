0abf4542ca4a6924491b198e2329da82f5484ad1
S1m1ster <darrylrobles14@gmail.com>
2022-04-24 05:29:46 -0400

    Completed lab's 6, 7, 8

--- Week1/Lab6/BonusSwitch.java
+++ Week1/Lab6/BonusSwitch.java
@@ -4,6 +4,39 @@ public class BonusSwitch {
 	public int[] resultBonuses;
 
 	public void bonus() {
+		int[] bonuses = new int[employeeLetters.length];
+		int i = 0;
+
+		do{
+			switch(employeeLetters[i]){
+
+				case 'A':
+				bonuses[i] = 5000;
+				i++;
+				break;
+
+				case 'B':
+				bonuses[i] = 2500;
+				i++;
+				break;
+
+				case 'C':
+				bonuses[i] = 1000;
+				i++;
+				break;
+
+				case 'D':
+				bonuses[i] = 500;
+				i++;
+				break;
+
+				case 'F':
+				bonuses[i] = 0;
+				i++;
+				break;
+
+			}
+		}while(i < employeeLetters.length);
 
 		// Code for the test runner, do not change
 		resultBonuses = bonuses;
