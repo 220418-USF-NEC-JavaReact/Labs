8b74d20a1b71170edade756e053f3cec2f8a7c98
jtsuch <37488788+jtsuch@users.noreply.github.com>
2022-04-25 13:09:56 -0700

    Saving the rest of Lab13 for after we learn Lambda

--- Week1/Lab13/LambdaLab.java
+++ Week1/Lab13/LambdaLab.java
@@ -12,5 +12,25 @@ interface GenericInterface<T> {
 public class LambdaLab {
 
     // Provide the logic for the lab below
+    public static Integer factorial(int n) {
+        int fact = 1;
+        for(int i = 2; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
 
+    public static String reverse(String s) {
+        String rev = "";
+        for(int i = s.length()-1; i >= 0; i--) {
+            rev += s.charAt(i);
+        }
+        return rev;
+    }
+
+    public List<String> forEachReverse(List<String> listString) {
+        List<String> sol = new ArrayList<String>();
+        listString.forEach((s) -> sol.add(reverse(s)));
+        return sol;
+    }
 }
