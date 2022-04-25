a77635aa5c3815403dee1efab16b9fc39166556f
jj-link <joseph.j.link@gmail.com>
2022-04-25 14:53:43 -0400

    Lab 7 success!

--- Week1/Lab7/StringMethods.java
+++ Week1/Lab7/StringMethods.java
@@ -1,3 +1,19 @@
 public class StringMethods {
 
+    public String replaceAllSpaces(String input) {
+        String output = input.replace(' ','-');
+        return output;
+    }
+
+    public boolean containsWord(String input, String searchWord) {
+        input = input.toLowerCase();
+        boolean output = input.contains(searchWord);
+        return output;
+    }
+
+    public String[] splitPhoneNumber(String phoneNumber) {
+        String[] output = phoneNumber.split("-");
+        return output;
+    }
+
 }
