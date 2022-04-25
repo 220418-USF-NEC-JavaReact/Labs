fc96eb64f629d5105bb063da3d85154371ddc228
Parth Kikani <kikaniparth@gmail.com>
2022-04-25 11:35:28 -0500

    lab 9 still in progress

---
+++ Week1/Lab9/Premium.java
@@ -0,0 +1,10 @@
+package Week1.Lab9;
+
+public interface Premium {
+
+    public static final String title = "Premium Customer";
+
+
+    public double discountPrice(double cost);
+    
+}
---
+++ Week1/Lab9/PremiumCustomer.java
@@ -0,0 +1,58 @@
+package Week1.Lab9;
+
+import java.util.Arrays;
+import Week1.Lab9.*;
+
+public class PremiumCustomer extends Customers implements Premium {
+
+    int vipCard;
+    int years;
+
+    public PremiumCustomer(String name, double balance, String[] cart, double cartCost, int vipCard, int years) {
+        super(name, balance, cart, cartCost);
+        this.name = name;
+        this.balance = balance;
+        this.cart = cart;
+        this.cartCost = cartCost;
+        this.vipCard = vipCard;
+        this.years = years;
+        
+
+        //TODO Auto-generated constructor stub
+    }
+
+    public int getVipCard(){return vipCard;}
+
+    public void setVipCard(int vipCard) {this.vipCard = vipCard;}
+    public int getYears(){return years;}
+
+    public void setYears(int years) {this.years = years;}
+
+
+    public double discountPrice(double cost) {
+
+        return cost * 0.85;
+    }
+
+    public void buy() {
+        setBalance(getBalance()-discountPrice(getCartCost()));
+        String[] newCart = new String[0];
+        setCart(newCart);
+        setCartCost(0.0);
+
+    }
+
+    public String toString(){
+        String objectString = "";
+        objectString += "Name: " + getName() + "\n";
+        objectString += "Balance: " + getBalance() + "\n";
+        objectString += "Cart: " + Arrays.toString(getCart()) + "\n";
+        objectString += "Cart Cost: " + getCartCost() + "\n";
+        objectString += "VIP Card: " + getVipCard() + "\n";
+        objectString += "Years Customer: " + getYears() +"\n";
+        return objectString;
+    }
+
+
+    
+}
