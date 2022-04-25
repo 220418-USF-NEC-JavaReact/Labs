e460975d29ece712a542d5c1491fdd7bf3f21802
swathij943 <swathij943@gmail.com>
2022-04-25 00:14:50 -0400

    Lab7

--- Week1/Lab7/StringMethods.java
+++ Week1/Lab7/StringMethods.java
@@ -1,3 +1,28 @@
 public class StringMethods {
 
+    public String replaceAllSpaces(String i){
+        char[] j = i.toCharArray();
+        for(int k = 0; k < i.length(); k++){
+            char l = i.charAt(k);
+            if (l == ' ') {
+                j[k] = '-';
+
+
+            }
+        } 
+        String m = String.valueof(j);
+        return m;
+
+    }
+    public boolean containsWord(String i, String j) {
+        String one = i.toLowerCase();
+        String two = j.toLowerCase();
+        return one.contains(two);
+
+    }
+    public String[] splitCellNumber(String i) {
+        String[] cell = a.split("_", 0);
+        return cell;
+    }
+
 }
