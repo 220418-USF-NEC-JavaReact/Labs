eab61bb3b6b03621ef65956a69c1d7584041bd2f
jj-link <joseph.j.link@gmail.com>
2022-04-24 21:32:34 -0400

    Lab 6 success!

--- Week1/Lab6/BonusSwitch.java
+++ Week1/Lab6/BonusSwitch.java
@@ -3,7 +3,29 @@ public class BonusSwitch {
 	public char[] employeeLetters;
 	public int[] resultBonuses;
 
+	// function which returns single bonus instance
+	int letterToBonus(char letterGrade){
+		switch (letterGrade) {
+			case 'A': return 5000;
+			case 'B': return 2500;
+			case 'C': return 1000;
+			case 'D': return 500;
+			case 'F': return 0;
+			default:
+					System.out.println("Invalid char entered");
+					return -1;
+		}
+	}
+
+
 	public void bonus() {
+		int i = 0;
+		int[] bonuses = new int[employeeLetters.length];
+		do {
+			bonuses[i] = letterToBonus(employeeLetters[i]);
+			i++;
+		}while (i<employeeLetters.length);
+
 
 		// Code for the test runner, do not change
 		resultBonuses = bonuses;
