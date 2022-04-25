07762808a2df9e0e4f1eba05fda30849f84e3916
jtsuch <37488788+jtsuch@users.noreply.github.com>
2022-04-25 09:45:35 -0700

    Almost done with Lab10, but has issue

--- Week1/Lab10/CollectionsFun.java
+++ Week1/Lab10/CollectionsFun.java
@@ -8,5 +8,33 @@ import java.util.Iterator;
 public class CollectionsFun {
 
 	// Fill out the class with the logic
+	public List<String> nameList(List<Person> people) {
+		List<String> names = new ArrayList<String>();
+		for(Person p : people) {
+			names.add(p.getFirst());
+		}
+		return names;
+	}
 
+	public Map<String, Person> personMap(List<Person> people) {
+		Map<String, Person> personMap = new HashMap<>();
+		for(Person p : people) {
+			personMap.put(p.getFirst() + " " + p.getLast(), p);
+		}
+		return personMap;
+	}
+
+	// Uncertain why this function is not entered, I will
+	// fix at a later date.
+	public Person iteratorFun(List<Person> people) {
+		Iterator<Person> fun = people.iterator();
+		Person oldest = people.get(0);
+		while(fun.hasNext()) {
+			Person curr = fun.next();
+			if(curr.getAge() > oldest.getAge()) {
+				oldest = curr;
+			}
+		}
+		return oldest;
+	}
 }
