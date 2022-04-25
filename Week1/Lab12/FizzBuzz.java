1f8713d2a4a3553b807e3087d17aea6fb316fc63
jtsuch <37488788+jtsuch@users.noreply.github.com>
2022-04-25 11:44:59 -0700

    Finished Lab12

--- Week1/Lab12/FizzBuzz.java
+++ Week1/Lab12/FizzBuzz.java
@@ -2,7 +2,15 @@ public class FizzBuzz {
 
     // Please place the logic to the coding problem in the method below
     public String fizzBuzz(int n) {
-
+        String sol = "";
+        if(n % 3 == 0) {
+            sol += "Fizz";
+        }
+        if(n % 5 == 0) {
+            sol += "Buzz";
+        }
+        if(sol.equals("")) return Integer.toString(n);
+        return sol;
     }
 
 }
