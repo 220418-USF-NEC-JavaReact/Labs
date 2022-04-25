d78ac4bdf970143edbc6648209e40be397cfcddb
nabilgj <nabilgj.bnm@gmail.com>
2022-04-25 09:59:06 -0400

    created Lab10

--- Week1/Lab10/CollectionsFun.java
+++ Week1/Lab10/CollectionsFun.java
@@ -1,12 +1,32 @@
-
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Iterator;
+
 
 public class CollectionsFun {
 
-	// Fill out the class with the logic
+ public static void main(String args[]){
+	 	// Fill out the class with the logic
+	public List<String> nameList(List<Person> person){
+
+		List<String> personList = new ArrayList<String>();
+		for (Person p : person){
+			personList.add(p.getFirst());
+		}
+		return personList;
+	}
+
+	public Map<String, Person> personMap(List<Person> person){
+		Map<String, Person> personMap = new HashMap<>();
+		String temp = "";
+		for(Person p: person){
+			temp = p.getFirst() + p.getLast();
+			personMap.put(temp, p);
+
+		}
+		return personMap;
+	}
+ }
 
 }
