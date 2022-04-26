321f9ea0b7283a0ae496783ef08f66f7a83631d6
jtsuch <37488788+jtsuch@users.noreply.github.com>
2022-04-26 14:32:19 -0700

    Finished Lab16

--- Week2/Lab16-C/Candles.java
+++ Week2/Lab16-C/Candles.java
@@ -3,7 +3,17 @@ public class Candles {
     //Provide the logic for the problem below
     //The candles array can be any size
     public int candles(int candles[]){
-
+        int maxHeight = 0;
+        int tallCount = 0;
+        for(int i = 0; i < candles.length; i++) {
+            if(candles[i] > maxHeight) {
+                tallCount = 1;
+                maxHeight = candles[i];
+            } else if (candles[i] == maxHeight) {
+                tallCount++;
+            }
+        }
+        return tallCount;
     }
 
 }
