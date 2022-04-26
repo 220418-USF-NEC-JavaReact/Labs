a55a32ab8a0561d521e8a8b59f343497175b7922
dawitsalemu <70491458+dawitsalemu@users.noreply.github.com>
2022-04-26 15:47:15 -0600

    add logic to fizzBuzz() which checks te divisiblity of a number by 3, 5 or both and print strings accordingly

---
+++ Week1/Lab12/Driver.java
@@ -0,0 +1,6 @@
+public class Driver {
+    public static void main(String args[]){
+        FizzBuzz num = new FizzBuzz();
+        System.out.println(num.fizzBuzz(10));
+    }
+}
--- Week1/Lab12/FizzBuzz.java
+++ Week1/Lab12/FizzBuzz.java
@@ -2,7 +2,28 @@ public class FizzBuzz {
 
     // Please place the logic to the coding problem in the method below
     public String fizzBuzz(int n) {
+        /**
+         * If the number N is divisible by 3, 5, both or none
+         * return "Fizz", "Buzz", "FizzBuzz" or the number correspondingly
+            If the number N is divisible by 5 return "Buzz".
+            If The number N is divisible by BOTH return "FizzBuzz"
+            If none of the above are true, return the number N as a string
+            
+            rearrange the ordering of the checks above to make the program work properly
+        */
 
+        if(n % 3 == 0 && n % 5 == 0){
+            return "FizzBuzz";
+        }
+        else if(n % 3 == 0){
+            return "Fizz";
+        }
+        else if(n % 5 == 0){
+            return "Buzz";
+        }
+        else{
+            return Integer.toString(n);
+        }
     }
 
 }
