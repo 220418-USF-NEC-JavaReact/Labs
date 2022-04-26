63e922fb27a3d36a2cb0a03466dc245015ddae43
Johnsoni17 <65513578+Johnsoni17@users.noreply.github.com>
2022-04-26 12:23:27 -0400

    finished Lab13

--- Week1/Lab13/LambdaLab.java
+++ Week1/Lab13/LambdaLab.java
@@ -7,10 +7,32 @@ interface GenericInterface<T> {
 
     // generic method
     T func(T t);
+
 }
 
 public class LambdaLab {
 
     // Provide the logic for the lab below
+
+    GenericInterface<Integer> factorial = (n) ->  {
+      int result = 1;
+
+      for(int i=1; i <= n; i++)
+        result = i * result;
+
+      return result;
+    };
+
+    GenericInterface<String> reverse = (s) ->{
+
+        StringBuffer sr = new StringBuffer(s);
+        return sr.reverse().toString();
+    };
+
+    public List<String> forEachReverse(List<String> ls){
+        List<String> rl = new ArrayList<>();
+        ls.forEach((s) -> {rl.add(reverse.func(s));});
+        return rl;
+    }
 
 }
