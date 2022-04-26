416316e1c10c1b0d658642f6840cb5b8c3d05c1e
unknown <pkiecun@gmail.com>
2022-04-25 23:29:59 -0400

    aprilTwentyFith

--- Week1/Lab12/FizzBuzz.java
+++ Week1/Lab12/FizzBuzz.java
@@ -2,7 +2,16 @@ public class FizzBuzz {
 
     // Please place the logic to the coding problem in the method below
     public String fizzBuzz(int n) {
-
+        if((n % 3) == 0){
+            if((n % 5) == 0){
+                return "FizzBuzz";
+            }else{
+                return "Fizz";
+            }
+        }else if((n % 5) == 0){
+            return "Buzz";
+        }
+            return Integer.toString(n);
     }
 
 }
--- Week1/Lab14/Race.java
+++ Week1/Lab14/Race.java
@@ -1,7 +1,62 @@
 public class Race {
 
     public String twoKidsAndATruck(int k1, int k2, int t) {
-
+        boolean kidOne = t > k1;
+        boolean kid1 = t < k1;
+        boolean kidTwo = t > k2;
+        boolean kid2 = t < k2;
+        boolean truckOne = t == k1;
+        boolean truckTwo = t == k2;
+        byte choice = 0;
+        String result;
+        if(kidOne){
+            k1 = t - k1;
+            if(kidTwo){ 
+                k2 = t - k2;
+                if(k1 == k2){
+                }else if(k1 > k2){
+                    choice = 2;}else if(k1 < k2){
+                        choice = 1;
+                    }
+        }else if(kid2){
+            k2 -= t;
+            if(k1 == k2){
+            }else if(k1 > k2){
+                choice = 2;}else if(k1 < k2){
+                    choice = 1;
+                }
+        }
+    }else if(kid1){
+        k1 -= t;
+        if(kidTwo){ 
+            k2 = t - k2;
+            if(k1 == k2){
+            }else if(k1 > k2){
+                choice = 2;}else if(k1 < k2){
+                    choice = 1;
+                }
+    }else if(kid2){
+        k2 -= t;
+        if(k1 == k2){
+        }else if(k1 > k2){
+            choice = 2;}else if(k1 < k2){
+                choice = 1;
+            }
     }
-
+}else if(truckOne){
+    choice = 1;
+}else if(truckTwo){
+    choice = 2;
+}else if(k1 == k2){}
+    switch(choice) {
+        case 0 : result = "Truck";
+            break;
+        case 1 : result = "Kid 1";
+            break;
+        case 2 : result = "Kid 2";
+            break;
+        default : result = "Fault";
+    }
+    return result;
+}
 }
