992fd8ec8121519c4be9d9f696c3fc3c9491816b
jtsuch <37488788+jtsuch@users.noreply.github.com>
2022-04-27 11:10:49 -0700

    Finished Lab18

--- Week2/Lab18-C/WordCount.java
+++ Week2/Lab18-C/WordCount.java
@@ -5,8 +5,16 @@ public class WordCount {
     
     //Provide the logic to the problem in the method below
     public Map<String, Integer> count(String input){
-        
-        
+        Map<String, Integer> found = new HashMap<>();
+        String[] words = input.split(" ");
+        for(int i = 0; i < words.length; i++) {
+            if(found.containsKey(words[i])) {
+                found.put(words[i], found.get(words[i])+1);
+            } else {
+                found.put(words[i], 1);
+            }
+        }
+        return found;
 
     }
 
