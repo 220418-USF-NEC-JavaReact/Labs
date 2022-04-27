6fd7b793cf4ea8f31b1c65cd86b07fdfbef33bf1
jtsuch <37488788+jtsuch@users.noreply.github.com>
2022-04-27 14:36:11 -0700

    Finished Lab19

--- Week2/Lab19/script.sql
+++ Week2/Lab19/script.sql
@@ -4,14 +4,14 @@ create table songs (
     name varchar(64) not null,
     artist varchar(64) not null,
     album varchar(64) not null,
-    duration decimal
+    duration numeric(5,2)
 );
 
-insert into (1, 'In the End', 'Linkin Park', 'Hybrid Theory', 3.62),
+insert into songs values (1, 'In The End', 'Linkin Park', 'Hybrid Theory', 3.62),
     (2, 'Kryptonite', '3 Doors Down', 'The Better Life', 3.9),
     (3, 'Play It Again', 'Luke Bryan', 'Crash My Party', 3.78),
     (4, 'Austin', 'Blake Shelton', 'Blake Shelton', 3.83),
     (5, 'Mr. Brightside', 'The Killers', 'Hot Fuss', 3.72),
-    (6, 'Stacy\'s Mom', 'Bowling For Soup', 'Ive Never Done Anything Like This', 3.22);
+    (6, 'Stacys Mom', 'Bowling For Soup', 'Ive Never Anything Like This', 3.22);
 
 select * from songs;
