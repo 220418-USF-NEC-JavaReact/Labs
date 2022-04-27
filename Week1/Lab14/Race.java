7da8eb1ff8021bbf1a24510d490e1c4cf8517c23
jtsuch <37488788+jtsuch@users.noreply.github.com>
2022-04-27 09:46:21 -0700

    Finished Lab14

--- Week1/Lab14/Race.java
+++ Week1/Lab14/Race.java
@@ -1,7 +1,14 @@
+import java.lang.Math;
 public class Race {
 
     public String twoKidsAndATruck(int k1, int k2, int t) {
-
+        if(Math.abs(k1 - t) < Math.abs(k2 - t)) {
+            return "Kid 1";
+        } else if (Math.abs(k1 - t) > Math.abs(k2 - t)) {
+            return "Kid 2";
+        } else {
+            return "Truck";
+        }
     }
 
 }
