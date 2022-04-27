8df0dd56bcb9a34638e8d27c6ac77ba10abbc987
moehaji <mo.ali.abdulla@gmail.com>
2022-04-27 12:26:00 -0400

    Lab13 Done

--- Week1/Lab13/LambdaLab.java
+++ Week1/Lab13/LambdaLab.java
@@ -12,5 +12,36 @@ interface GenericInterface<T> {
 public class LambdaLab {
 
     // Provide the logic for the lab below
+    GenericInterface<Integer> factorial = (num) -> {
+        int fact = 1;
+        for (int i = 2; i <= num; i++) {
+            fact = fact * i;
+        }
+        
+        return fact;
+    };
+
+    GenericInterface<String> reverse = (str) -> {
+        String reversedStr = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversedStr += str.charAt(i);
+        }
+
+        return reversedStr;
+    };
+
+    public List<String> forEachReverse(List<String> strList) {
+        List<String> reversedStrList = new ArrayList<>();
+        StringBuilder reversedString = new StringBuilder();
+
+        strList.forEach(str -> {
+            reversedString.append(str);
+            reversedString.reverse();
+            reversedStrList.add(reversedString.toString());
+            reversedString.delete(0, reversedString.length());
+        });
+
+        return reversedStrList;
+    }
 
 }
