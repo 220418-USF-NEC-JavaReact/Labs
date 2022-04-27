c217659651a62ee37694cddf5512d17630ea1500
Milan Verma <millan.verma@gmail.com>
2022-04-26 18:15:24 -0600

    Lab 12 completed

--- Week1/Lab12/FizzBuzz.java
+++ Week1/Lab12/FizzBuzz.java
@@ -2,7 +2,17 @@ public class FizzBuzz {
 
     // Please place the logic to the coding problem in the method below
     public String fizzBuzz(int n) {
+        String res = "";
+        if(n%3==0 && n%5==0){
+           res = "FizzBuzz";
+        } else if(n%5==0){
+            res = "Buzz";
+        } else if(n%3==0){
+            res = "Fizz";
+        } else{
+            res = Integer.toString(n);
+        }
 
+        return res;
     }
-
 }
