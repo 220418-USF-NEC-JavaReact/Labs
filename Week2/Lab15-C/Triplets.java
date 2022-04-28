c84335bf1a84721d6feab90ecf59d250c1793c8f
Milan Verma <millan.verma@gmail.com>
2022-04-28 16:31:12 -0600

    Lab15-C Completed

--- Week2/Lab15-C/Triplets.java
+++ Week2/Lab15-C/Triplets.java
@@ -1,9 +1,16 @@
-public class Triplets{
+public class Triplets {
 
     //Provide the logic to the problem in the method below
     //a1 and a3 will always be 3 in length
-    public int[] compareTriplets(int[] a, int[] b){
-
+    public int[] compareTriplets(int[] a, int[] b) {
+        int[] points = {0, 0};
+        for (int i = 0; i < 3; i++) {
+            if (a[i] > b[i]) {
+                points[0]++;
+            } else if (a[i] < b[i]) {
+                points[1]++;
+            }
+        }
+        return points;
     }
-
 }
