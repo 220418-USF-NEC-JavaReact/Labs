0adbd7781032d702e5964b225e4f8559c0b451e9
bayodes <bayode.o.olaoye@gmail.com>
2022-04-28 17:58:59 -0400

    Completed Lab16

--- Week2/Lab16-C/Candles.java
+++ Week2/Lab16-C/Candles.java
@@ -3,7 +3,25 @@ public class Candles {
     //Provide the logic for the problem below
     //The candles array can be any size
     public int candles(int candles[]){
+        int tallest = 0;
+        int temp = candles[0];
+        int counter = 0;
 
+        for (int i = 0; i < candles.length; i++) {
+            if (candles[i] > temp) {
+                temp = candles[i];
+            }
+        }
+
+        for (int k = 0; k < candles.length; k++) {
+            if(temp == candles[k]) {
+                counter++;
+            }
+        }
+
+        tallest = counter;
+
+        return tallest;
     }
 
 }
