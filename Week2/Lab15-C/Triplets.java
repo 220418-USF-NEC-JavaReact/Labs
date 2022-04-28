e3a9c883afb29500d359955e2502b618d77d08b4
bayodes <bayode.o.olaoye@gmail.com>
2022-04-28 12:52:08 -0400

    Completed Lab15

--- Week2/Lab15-C/Triplets.java
+++ Week2/Lab15-C/Triplets.java
@@ -3,7 +3,16 @@ public class Triplets{
     //Provide the logic to the problem in the method below
     //a1 and a3 will always be 3 in length
     public int[] compareTriplets(int[] a, int[] b){
-
-    }
+        int[] newArray = new int[a.length];
+        
+        for (int i=0; i <a.length; i++) {
+            if(a[i] > b[i]) {
+                newArray[0] += 1;
+            } else if (a[i] < b[i]) {
+                newArray[1] += 1;
+            }
+        }
+        return newArray;
+    } 
 
 }
