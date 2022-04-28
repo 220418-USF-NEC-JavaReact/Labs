a32b1aa3fe4da49d7c3681c8b73e95c89b755641
Milan Verma <millan.verma@gmail.com>
2022-04-28 17:31:56 -0600

    Lab16-C Complete

--- Week2/Lab16-C/Candles.java
+++ Week2/Lab16-C/Candles.java
@@ -1,9 +1,22 @@
+import java.util.Arrays;
+
 public class Candles {
 
     //Provide the logic for the problem below
     //The candles array can be any size
     public int candles(int candles[]){
-
+        Arrays.sort(candles);
+        int len = candles.length-1;
+        int tallest = candles[len];
+        int res = 0;
+        try{
+            while(tallest==candles[len]){
+                res++;
+                len--;
+            }
+        } catch (IndexOutOfBoundsException e){
+            //Ignore
+        }
+        return res;
     }
-
 }
