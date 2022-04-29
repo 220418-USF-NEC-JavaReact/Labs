9c3830d48b5d2deb63791e224995fbd17ead49ee
moehaji <mo.ali.abdulla@gmail.com>
2022-04-28 22:55:56 -0400

    Lab16 Done

--- Week2/Lab16-C/Candles.java
+++ Week2/Lab16-C/Candles.java
@@ -3,7 +3,23 @@ public class Candles {
     //Provide the logic for the problem below
     //The candles array can be any size
     public int candles(int candles[]){
+        int max = 0;
+        int instancesOfMax = 0;
 
+
+        for(int i = 0; i<candles.length; i++) {
+            if(candles[i] > max) {
+                max = candles[i];
+            }
+        }
+
+        for(int i = 0; i<candles.length; i++) {
+            if (candles[i] == max) {
+                instancesOfMax += 1;
+            }
+        }
+
+        return instancesOfMax;
     }
 
 }
