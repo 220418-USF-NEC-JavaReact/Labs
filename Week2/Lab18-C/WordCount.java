7ed5455f41ba55b2b5009561d7a89d21f5ca71da
Milan Verma <millan.verma@gmail.com>
2022-04-28 18:00:23 -0600

    Lab18-C Complete

--- Week2/Lab18-C/WordCount.java
+++ Week2/Lab18-C/WordCount.java
@@ -2,12 +2,26 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class WordCount {
-    
-    //Provide the logic to the problem in the method below
-    public Map<String, Integer> count(String input){
-        
-        
 
+    //Provide the logic to the problem in the method below
+    public Map<String, Integer> count(String input) {
+        Map<String, Integer> count = new HashMap<>();
+        String term = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != ' ') {
+                term += input.charAt(i);
+            } else {
+                if (!count.containsKey(term)) {
+                    count.put(term, 1);
+                } else {
+                    count.put(term, count.get(term) + 1);
+                }
+                term = "";
+            }
+        }
+        return count;
     }
+
 
 }
