19577c2afbecf9a890f40e6bd0e60f7f0be27fd6
jtsuch <37488788+jtsuch@users.noreply.github.com>
2022-04-29 09:41:03 -0700

    Finished Lab21

--- Week2/Lab21/script.sql
+++ Week2/Lab21/script.sql
@@ -7,6 +7,23 @@ from employee
 right join department on department = department_id;
 
 select employee_name, city
-from employee
-left join location on location = location_id;
+from location
+left join employee on location = location_id;
 
+select *
+from employee where location = 1
+union
+select *
+from employee where department = 1;
+
+select *
+from employee where department = 1
+except
+select *
+from employee where location = 1;
+
+select *
+from employee where department = 1
+intersect
+select *
+from employee where department = 1;
