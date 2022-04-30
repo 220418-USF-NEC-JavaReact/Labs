28f8c51b5e059e3a614b52b7f394cd0b9c9b044a
bayodes <bayode.o.olaoye@gmail.com>
2022-04-29 21:00:50 -0400

    Completed Lab18

--- Week2/Lab18-C/WordCount.java
+++ Week2/Lab18-C/WordCount.java
@@ -5,9 +5,18 @@ public class WordCount {
     
     //Provide the logic to the problem in the method below
     public Map<String, Integer> count(String input){
-        
-        
+        String[] s = input.split(" ");
+        Map<String, Integer> res = new HashMap<>();
 
+        for(int i = 0; i < s.length; i++) {
+            if (res.containsKey(s[i])){
+               res.put(s[i], res.get(s[i])+1);
+            }else{
+               res.put(s[i], 1);
+           }
+        }
+    
+        return res;
     }
 
 }
