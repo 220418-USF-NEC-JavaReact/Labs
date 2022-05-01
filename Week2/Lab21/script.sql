5806fea1d89ede0a6275db363802787038ca08d8
Milan Verma <millan.verma@gmail.com>
2022-05-01 10:42:33 -0600

    Completed lab20

---
+++ Week2/Lab21/script.sql
@@ -0,0 +1,29 @@
+SELECT employee.employee_name, department.department_name FROM employee 
+INNER JOIN department 
+ON department.department_id = employee.department;
+
+SELECT employee.employee_name, department.department_name FROM employee 
+RIGHT JOIN department 
+ON department.department_id = employee.department;
+
+SELECT employee.employee_name, location.city FROM location 
+LEFT JOIN employee 
+ON location.location_id = employee.location;
+
+SELECT * FROM employee
+WHERE department=1
+UNION
+SELECT * FROM employee 
+WHERE location =1;
+
+SELECT * FROM employee
+WHERE department=1
+MINUS
+SELECT * FROM employee 
+WHERE location =1;
+
+SELECT * FROM employee
+WHERE location =1 OR department=1
+INTERSECT
+SELECT * FROM employee 
+WHERE department =1;
